523a4bda214c2cf3ea3be7da64f8c43814787ed9
Mark Blanford <markytorture@hotmail.com>
2018-02-13 15:43:17 +0000

    add item to basket test passed

--- basket.js
+++ basket.js
@@ -7,5 +7,8 @@ Basket.prototype.getItemsCount = function(){
   return this.items.length;
 }
 
+Basket.prototype.addItem = function(item){
+  this.items.push(item);
+}
 
 module.exports = Basket;
--- specs/basket_spec.js
+++ specs/basket_spec.js
@@ -1,15 +1,19 @@
 const assert = require("assert");
 const Basket = require("../basket.js")
+const Item = require("../item.js")
 
 
 describe("Basket", function(){
 
   let basket;
+  let item1;
+  let item2;
 
   beforeEach(function(){
 
     basket = new Basket();
-
+    item1 = new Item('Oreos', 0.89, true);
+    item2 = new Item('Curcumin', 9.10, false);
   })
 
   it('should start with an empty array of items', function(){
@@ -17,7 +21,12 @@ describe("Basket", function(){
     assert.equal(actual, 0);
   });
 
-
+  it('should add items to basket', function(){
+    basket.addItem(item1);
+    basket.addItem(item2);
+    const actual = basket.getItemsCount();
+    assert.strictEqual(actual, 2);
+  })
 
 
 
